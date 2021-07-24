127da66b8e2b0aa30a275b1e2e3bd503680283d1
Swati Latthwal <swati.lathwal@gmail.com>
2021-07-24 01:32:50 -0400

    initial commit

--- dataframe/ingestion/others/systems/mysql_df.py
+++ dataframe/ingestion/others/systems/mysql_df.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 
     # use the ** operator/un-packer to treat a python dictionary as **kwargs
     print("\nReading data from MySQL DB using SparkSession.read.format(),")
-    txnDF = spark\
+    txnDF = SparkSession\
         .read.format("jdbc")\
         .option("driver", "com.mysql.cj.jdbc.Driver")\
         .options(**jdbc_params)\
